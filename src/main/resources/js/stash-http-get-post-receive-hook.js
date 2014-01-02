77749629ca8493eb0bac808101e8697117aaf5de
aeffle <mail@aeffle.de>
2014-01-02 21:54:34 +0100

    now with toggeling enabled status for the user and pass text fields

--- src/main/resources/js/stash-http-get-post-receive-hook.js
+++ src/main/resources/js/stash-http-get-post-receive-hook.js
@@ -1,18 +1,71 @@
+var aeffle = {};
+
+aeffle.getTextInput = function (id) {
+    'use strict';
+    var input, inputName = {
+        url: 'input#url',
+        useAuth: 'input#useAuth',
+        user: 'input#user',
+        pass: 'input#pass'
+    };
+
+    if (id > 1) {
+        inputName.url += id;
+        inputName.useAuth += id;
+        inputName.user += id;
+        inputName.pass += id;
+    }
+
+    input = {
+        url: AJS.$(inputName.url),
+        useAuth: AJS.$(inputName.useAuth),
+        user: AJS.$(inputName.user),
+        pass: AJS.$(inputName.pass)
+    };
+
+    input.setInputEditableStatus = function (isEnabled) {
+        var classAttribute = (isEnabled ? 'text disabled' : 'text'),
+            disabledAttribute = (isEnabled ? null : 'true');
+
+        this.user.attr('disabled', disabledAttribute).attr('class', classAttribute);
+        this.pass.attr('disabled', disabledAttribute).attr('class', classAttribute);
+    };
+    input.enable = function () {input.setInputEditableStatus(true); };
+    input.disable = function () {input.setInputEditableStatus(false); };
+
+    input.update = function () {
+        if (input.useAuth.prop('checked')) {
+            input.enable();
+        } else {
+            input.disable();
+        }
+    };
+
+    return input;
+};
+
+aeffle.addChangeEventTo = function (id) {
+    'use strict';
+    var input = aeffle.getTextInput(id);
+    input.useAuth.on('change', input.update);
+    input.useAuth.trigger('change');
+    input.url.css('max-width', '500px');
+};
 
 
 
 
 
-function showSavedData(){
-    var url = AJS.contextPath() + 
+aeffle.showSavedData = function () {
+    'use strict';
+    var url = AJS.contextPath() +
               "/rest/api/1.0/projects/PROJECT_1/repos/rep_1" +
-              "/settings/hooks" + 
+              "/settings/hooks" +
               "/de.aeffle.stash.plugin.stash-http-get-post-receive-hook:http-get-post-receive-hook" +
-		      "/settings";
-			   
-    AJS.$.getJSON(url, function(data) {
-        niceString = JSON.stringify(data, null, 4);
+              "/settings";
+
+    AJS.$.getJSON(url, function (data) {
+        var niceString = JSON.stringify(data, null, 4);
         alert(niceString);
     });
-}
-
+};
